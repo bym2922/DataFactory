f5895c6b1d03275461cc2c01d728b2aa95a5c940
iFan <13863684079@163.com>
2018-08-07 17:07:41 +0800

    添加了按行获取数据功能

--- factory/views.py
+++ factory/views.py
@@ -34,45 +34,43 @@ def table_basic(request):
 
     file_list = File.objects.all().order_by('-id')[start:end]
     print(len)
-    return render(request, "table_basic.html", {'file_list': file_list, 'len': total, 'pages': range(pages)})
+    data = {
+            'file_list': file_list,
+            'total': total,
+            'pages': range(pages),
+            }
+    return render(request, "table_basic.html", {'data': data})
 
 
-# @login_required
-def chart_columnar(request):
-    fname = request.GET.get('fname')
-    data = get_data(fname)
-    # return render(request, "chart_columnar.html", {'data': data})
-    return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
-
-
-# @login_required
 def chart_line(request):
     fname = request.GET.get('fname')
-    data = get_data(fname)
-    # hearder = {
-    #     'Access_Control_Allow_Credentials': 'true',
-    #     'Access_Control_Allow_Origin': 'http://192.168.1.177'
-    # }
+    # data = get_data(fname)
+    # data = get_data1(fname)
+    data = get_data2(fname)
     # # return render(request, "chart_line.html", {'data': data})
-    # response = HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
-    # response['Access_Control_Allow_Credentials'] = True
-    # response['Access_Control_Allow_Origin'] = 'http://192.168.1.177'
     return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
-    # return response
 
 
-# @login_required
+def chart_columnar(request):
+    fname = request.GET.get('fname')
+    # data = get_data(fname)
+    data = get_data1(fname)
+    return render(request, "chart_columnar.html", {'data': data})
+    # return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
+
+
 def chart_pie(request):
     fname = request.GET.get('fname')
-    data = get_data(fname)
+    # data = get_data(fname)
+    data = get_data1(fname)
     return render(request, "chart_pie.html", {'data': data})
     # return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
 
 
-# @login_required
 def chart_scatter(request):
     fname = request.GET.get('fname')
-    data = get_data(fname)
+    # data = get_data(fname)
+    data = get_data1(fname)
     return render(request, "chart_scatter.html", {'data': data})
     # return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
 
@@ -83,24 +81,30 @@ def table_cmplete(request):
     page = int(request.GET.get('page', 1))  # 页码
     per_page = 15
     request.session['fname'] = fname
-    data = clear_data(fname)
-    len = data.__len__()
+    excel_data = clear_data(fname)
+    len = excel_data.__len__()
     pages = ceil(len / per_page)  # 总页数
 
     start = (page - 1) * per_page
     end = start + per_page
     if end >= len:
         end = len
-    a = data.index.tolist()
+    a = excel_data.index.tolist()
     b = []
     data1 = dict()
     for i in range(start, end):
-        b.append(data.iloc[i].values.tolist())
+        b.append(excel_data.iloc[i].values.tolist())
     for i, j in zip(a, b):
         data1.update({str(i+1): j})
-
-    data2 = {'xxx': data.columns.tolist()}
-    return render(request, "table_complete.html", {'data': data1, 'data2': data2, 'pages': range(pages), 'fname': fname, 'len': len})
+    data2 = {'xxx': excel_data.columns.tolist()}
+    data = {
+            'data': data1,
+            'data2': data2,
+            'pages': range(pages),
+            'fname': fname,
+            'len': len,
+            }
+    return render(request, "table_complete.html", {'data': data})
 
 
 @login_required
@@ -150,14 +154,34 @@ def file_upload(request):
     else:
         return render(request, "file_upload.html")
 
-#
-# def get_data(request):
-#     fname = request.GET.get('fname')
-#     data = get_data(fname)
-#     return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
+
+# 处理表格数据, 按列获取
+def get_data1(fname):
+    data = clear_data(fname)
+    data1 = dict()
+    print(list(data))
+    for i in range(len(data.columns)):
+        data1.update({data.columns[i]: data[data.columns[i]].values.tolist()})
+    print(data1)
+    return data1
+    # return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json')
 
 
-# 读取表格数据, 按行获取
+def get_data2(fname):
+    data = clear_data(fname)
+    data1 = dict()
+    print(list(data))
+    for i in range(len(data.columns)):
+        data1.update({data.columns[i]: data[data.columns[i]].values.tolist()})
+    print(data1)
+    # print(data)
+    dd = {'Name': list(data),
+          'Data': data1,
+          }
+    return dd
+
+
+# 处理表格数据, 按行获取
 def get_data(fname):
     f = File.objects.get(fname=fname).fpath
     if f:
@@ -183,16 +207,10 @@ def get_data(fname):
     return totalArray
 
 
-# 对读取到的数据进行处理，为json格式
+# 读取表格
 def clear_data(fname):
     f = File.objects.get(fname=fname).fpath
     if f:
         data = pd.read_excel(f)
-        # data = get_data(fname)
-        # data1 = dict()
-        # for i in range(len(data.columns)):
-        #     data1.update({data.columns[i]: data[data.columns[i]].values.tolist()})
-        # print(type(data1))
-        # # return HttpResponse(json.dumps(data1), content_type='application/json')
-        # print(data1)
         return data
+
